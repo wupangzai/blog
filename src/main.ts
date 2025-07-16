8df646379d88162d9fbc49717362451c7205ad3c
an_an <1530765818@qq.com>
2025-07-16 15:43:55 +0800

    feat: 添加vue-router

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import registerDirectives from '@/directives';
 
 import { initApp } from '@/components/common/dialog';
 
+import router from '@/router';
+
 import App from './App.vue';
 
 const app = createApp(App);
@@ -15,6 +17,8 @@ const app = createApp(App);
 app.use(packages); // 三方库的引入
 
 app.use(registerDirectives); // 注册自定义指令
+
+app.use(router);
 
 initApp(app);
 
