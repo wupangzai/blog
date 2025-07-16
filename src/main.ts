82be8f7e5abbe88c100fd576893e0feecfe98dbd
an_an <1530765818@qq.com>
2025-07-16 15:05:09 +0800

    core: 添加 normalize.css

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
 
-createApp(App).mount("#app");
+import '@/assets/css/index.less';
+
+import App from './App.vue';
+
+createApp(App).mount('#app');
