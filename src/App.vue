d4a3efa6e4edd066b518c4bde2b222ad1b26b8e4
an_an <1530765818@qq.com>
2025-07-16 15:40:17 +0800

    feat: 添加自定义指定，v-custom-loading

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,4 @@
   <div><el-button>123</el-button></div>
 </template>
 
-<script setup lang="ts">
-import { useDialog } from './hooks';
-
-useDialog({
-  content: '123',
-});
-</script>
+<script setup lang="ts"></script>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 
 import '@/assets/css/index.less';
 
-import packages from '@/packages'; // 三方库的引入
+import packages from '@/packages';
+
+import registerDirectives from '@/directives';
 
 import { initApp } from '@/components/common/dialog';
 
@@ -10,7 +12,9 @@ import App from './App.vue';
 
 const app = createApp(App);
 
-app.use(packages);
+app.use(packages); // 三方库的引入
+
+app.use(registerDirectives); // 注册自定义指令
 
 initApp(app);
 
