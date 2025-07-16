b4bdd4fd8dbb1e1d7f8a0d401b25471a3abecab2
an_an <1530765818@qq.com>
2025-07-16 16:34:06 +0800

    fix: 修复打包中的ts import type error

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
+import type { InjectionKey } from 'vue';
+import type { EnhancedStore } from './types/root';
+import type { RootState } from '@/store/modules/root/types';
 import { createStore, Store as VuexStore, useStore as useBaseStore } from 'vuex';
-import { InjectionKey } from 'vue';
 import commonModule from './modules/common';
-import { EnhancedStore } from './types/root';
-import { RootState } from '@/store/modules/root/types';
 import { rootState, gettersInRoot, mutationsInRoot, actionsInRoot } from '@/store/modules/root';
 
 const key: InjectionKey<VuexStore<RootState>> = Symbol(); // 创建一个具备类型安全的唯一注入 Key，用于在 Vue 组件中注入 Store 实例。
